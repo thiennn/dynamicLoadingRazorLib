398b84123a25c5030557ace40e60987d02ba1518
Thien Nguyen <nlqthien@gmail.com>
2018-09-15 04:56:15 +0700

    tested with view components

---
+++ Plugin1/Areas/Plugin1/Components/MyViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Plugin1.Areas.Plugin1.ViewModels;
+
+namespace Plugin1.Areas.Plugin1.Components
+{
+    public class MyViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var model = new MyViewModel
+            {
+                Id = 20,
+                Name = "Awesome"
+            };
+
+            return View(model);
+        }
+    }
+}
